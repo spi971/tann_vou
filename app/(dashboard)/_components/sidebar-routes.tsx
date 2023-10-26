9bb397f645f5629cd0d9032320dedc2d31713a16
spi971 <sandypierre@protonmail.com>
2023-10-26 17:31:59 +0200

    Add:teacher mode implemented

--- app/(dashboard)/_components/sidebar-routes.tsx
+++ app/(dashboard)/_components/sidebar-routes.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { Compass, Layout } from "lucide-react";
+import { BarChart, Compass, Layout, List } from "lucide-react";
+import { usePathname } from "next/navigation";
 import SidebarItem from "./sidebar-item";
 
 const guestRoutes = [
@@ -15,8 +16,23 @@ const guestRoutes = [
     href: "/search",
   },
 ];
+
+const teacherRoutes = [
+  {
+    icon: List,
+    label: "Courses",
+    href: "/teacher/courses",
+  },
+  {
+    icon: BarChart,
+    label: "Analytics",
+    href: "/teacher/analytics",
+  },
+];
 const SideBarRoutes = () => {
-  const routes = guestRoutes;
+  const pathname = usePathname()
+  const isTeacherPage = pathname?.includes("/teacher");
+  const routes = isTeacherPage ? teacherRoutes: guestRoutes;
   return (
     <div className='flex flex-col w-full'>
         {routes.map((route)=>{
--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -1,10 +1,34 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import { UserButton } from "@clerk/nextjs";
+import { LogOut } from "lucide-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const NavbarRoutes = () => {
+  const pathname = usePathname();
+
+  const isTeacherPage = pathname?.startsWith("/teacher");
+  // to show the chapters list instead of th classic side menu
+  const isPlayerPage = pathname?.includes("/chapter");
+
   return (
     <div className='flex gap-x-2 ml-auto'>
+      {isTeacherPage || isPlayerPage ? (
+        <Link href='/'>
+          <Button size='sm' variant='ghost'>
+            <LogOut className='h-4 w-4 mr-2' />
+            exit
+          </Button>
+        </Link>
+      ) : (
+        <Link href='/teacher/courses'>
+          <Button size='sm' variant='ghost'>
+            Teacher mode
+          </Button>
+        </Link>
+      )}
       <UserButton afterSignOutUrl='/' />
     </div>
   );
